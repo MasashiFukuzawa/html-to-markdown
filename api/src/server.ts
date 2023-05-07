a4af8b7dd9948aa50e34e326273e9d7a2a95f2d2
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2023-05-07 14:40:59 +0900

    feat: implement logic which convert html to markdown

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,50 @@
 import express, { Request, Response } from 'express';
+import { JSDOM } from 'jsdom';
+import fetch from 'node-fetch';
+import TurndownService from 'turndown';
 
 const app = express();
 
 const port = 8080;
 
-app.get('/api', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World!' });
+app.use(express.json());
+
+app.get('/api', async (req: Request, res: Response) => {
+  const url = req.query.url as string | undefined;
+
+  if (!url) {
+    res.status(400).send('Missing url parameter');
+    return;
+  }
+
+  const response = await fetch(url);
+  const html = await response.text();
+  const { window } = new JSDOM(html);
+  const { document } = window;
+
+  const body = document.body;
+
+  const tags = ['header', 'footer', 'style', 'script', 'noscript'];
+
+  for (const tag of tags) {
+    const elements = body.getElementsByTagName(tag);
+    for (let i = elements.length - 1; i >= 0; i--) {
+      const parentNode = elements[i].parentNode;
+      if (parentNode) {
+        elements[i].parentNode?.removeChild(elements[i]);
+      }
+    }
+  }
+
+  const turndownService = new TurndownService();
+  const markdown = turndownService.turndown(body.innerHTML);
+
+  const markdownUrlRegex = /\[(.*?)\]\((https?:\/\/[^\s\)]+)\)/g;
+  const trimmedMarkdown = markdown.replace(markdownUrlRegex, '[$1]()');
+
+  res.status(200).json({ markdown: trimmedMarkdown });
 });
 
 app.listen(port, () => {
-  console.log(`html-to-markdown app listening at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
