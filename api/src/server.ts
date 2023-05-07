d2fa08100e9a619d69035667d8625ed5474a1dcd
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2023-05-07 13:43:49 +0900

    chore: use typescript

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,10 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 const port = 8080;
 
-app.get('/api', (req, res) => {
+app.get('/api', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' });
 });
 
